e61c884921df4def8260b3dce92a1dc84d0035a3
とうふ <72054016+dangotofu@users.noreply.github.com>
2023-01-09 18:14:12 -0700

    something

---
+++ equal_parenthesis.py
@@ -0,0 +1,10 @@
+def even_parenthesis(s: str) -> bool:
+    stack = []
+    for ch in s:
+        if ch == '(':
+            stack.append(ch)
+        elif ch == ')':
+            if not stack:
+                return False
+            stack.pop()
+    return not stack
